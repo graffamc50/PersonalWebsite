f29bdaae1ea557d42e2b2bbfb6f21b2a33b9f9b3
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-06-09 16:55:00 -0700

    Night / Day Feature Complete

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+const switchTheme = () => {
+    const rootElem = document.documentElement
+    let colorTheme = rootElem.getAttribute('color-theme'),
+        newTheme
+    
+    newTheme = (colorTheme === "light") ? "dark" : "light"
+    rootElem.setAttribute('color-theme', newTheme)
+}
+
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
