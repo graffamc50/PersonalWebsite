a1161bebf5becd1b25c2f412f59d3f654065def0
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-06-09 17:49:06 -0700

    Day / Night Transition Timing

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const switchTheme = () => {
     const rootElem = document.documentElement
     let colorTheme = rootElem.getAttribute('color-theme'),
         newTheme
-    
+
     newTheme = (colorTheme === "light") ? "dark" : "light"
     rootElem.setAttribute('color-theme', newTheme)
 }
